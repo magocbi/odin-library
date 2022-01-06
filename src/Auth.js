5d1bf5800ae4a16fb76d9ae2fa8c7dc7aec2619c
Diego <newskyforge2@hotmail.com>
2022-01-06 15:10:27 -0600

    Add firebase auth functions

---
+++ src/Auth.js
@@ -0,0 +1,32 @@
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+export async function signInUser() {
+  const provider = new GoogleAuthProvider();
+  await signInWithPopup(getAuth(), provider);
+}
+
+export function signOutUser() {
+  signOut(getAuth());
+}
+
+export function initFireBaseAuth(stateChangeHandler) {
+  onAuthStateChanged(getAuth(), stateChangeHandler);
+}
+
+export function getProfilePicUrl() {
+  return getAuth().currentUser.photoURL || null;
+}
+
+export function getUserName() {
+  return getAuth().currentUser.displayName;
+}
+
+export function isUserSignedIn() {
+  return !!getAuth().currentUser;
+}
